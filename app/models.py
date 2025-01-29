935d2e3fd4ef2807927a4260548416c04e65bd8e
Sharique <shariquereza786@gmail.com>
2025-01-29 11:45:30 +0530

    made feedback modelfor contactus page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class feedback(models.Model):
+    emp_name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=200,null=True)
+    subjects=models.CharField(max_length=300,null=True)
+    message=models.TextField()
+    action_taken=models.TextField()
