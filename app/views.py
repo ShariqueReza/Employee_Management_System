2fdef958a574aeac07be7ce44ef751c556af708d
Sharique <shariquereza786@gmail.com>
2025-01-29 11:25:20 +0530

    added contactus url & function

--- app/views.py
+++ app/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_page(request):
-    print(request)
     context = {}
     return render(request, 'app/index.html', context)
+
+def contactus_page(request):
+    context = {}
+    return render(request, 'app/contactus.html', context)
