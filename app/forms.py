43b9e821ad67acc75a2a455a9a1f645273400019
Sharique <shariquereza786@gmail.com>
2025-01-29 12:21:21 +0530

    added widgets in feedback form for styling & placeholder

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,25 @@ class FeedbackForm(forms.ModelForm):
     class Meta:
         model=feedback
         fields={'emp_name','email','subjects','message'}
+        widgets = {
+            'emp_name': forms.TextInput(attrs={
+                'id': 'name',
+                'placeholder': 'Employee Name',
+                'class': 'form-control mb-2 w-75 mx-auto inputsize'
+            }),
+            'email': forms.EmailInput(attrs={
+                'id': 'email',
+                'placeholder': 'Email Address',
+                'class': 'form-control mb-2 w-75 mx-auto inputsize'
+            }),
+            'subject': forms.TextInput(attrs={
+                'id': 'subject',
+                'placeholder': 'Subject',
+                'class': 'form-control mb-2 w-75 mx-auto inputsize'
+            }),
+            'message': forms.Textarea(attrs={
+                'id': 'message',
+                'placeholder': 'Your Message',
+                'class': 'form-control mb-2 w-75 mx-auto inputsize'
+            }),
+        }
