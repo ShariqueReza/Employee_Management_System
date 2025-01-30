6bc88465bb4455e965340df28bd872a5edddbaa3
Sharique <shariquereza786@gmail.com>
2025-01-30 12:51:35 +0530

    made departmentform

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from app.models import Departments
 
+
+class DepartmentForm(forms.ModelForm):
+    class Meta:
+        model=Departments
+        fields={'department_name','department_status'}
 
 
