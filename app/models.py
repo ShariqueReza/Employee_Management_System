cd6688444827421009dc436f9d4cbd0bfac60bf2
Sharique <shariquereza786@gmail.com>
2025-01-30 21:32:59 +0530

    made Payroll model

--- app/models.py
+++ app/models.py
@@ -15,3 +15,11 @@ class Employees(models.Model):
       department_name = models.ForeignKey(Departments, on_delete=models.CASCADE)
       role = models.CharField(max_length=100)
 
+class Payroll(models.Model):
+      emp_name = models.ForeignKey(Employees, on_delete=models.CASCADE)
+      role = models.CharField(max_length=100)
+      basic_salary = models.DecimalField(max_digits=10, decimal_places=2)
+      mobile_number = models.CharField(max_length=15)
+      tax = models.DecimalField(max_digits=5, decimal_places=2)
+      net_salary = models.DecimalField(max_digits=10, decimal_places=2)
+
